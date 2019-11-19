bccd49d12087045f06d770c53aa471bcf71f2338
DESKTOP-5K41LSK <mahnoor2809@gmail.com>
2019-11-19 23:02:35 +0500

    Python Code

---
+++ ques5.py
@@ -0,0 +1,9 @@
+st1 = str(input("Enter Something "))
+st2 = str(input("Enter Something "))
+st = False
+if (len(st1) == len(st2)):
+    print("True")
+    print(" Both String have same length ")
+else:
+   print("False") 
+   print(" Different Length ")    
